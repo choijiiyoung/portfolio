42612037ee3dc055beed2ebd142d87b1baef68fa
choijiiyoung <blueberryxchoco@gmail.com>
2023-06-07 15:12:57 +0900

    데이터 fetch 소스 수정(async)

--- js/department.js
+++ js/department.js
@@ -1,53 +1,107 @@
+// fetch('DB/department.json')
+// 	.then((res) => {
+// 		return res.json();
+// 	})
+// 	.then((data) => {
+// 		//member data
+// 		const memberData = data.members;
+
+// 		if (memberData.length > 0) {
+// 			tags = '';
+// 			memberData.map((data) => {
+// 				tags += `
+// 					<article>
+// 						<div class='pic'>
+// 							<img src='img/department/${data.pic}' />
+// 						</div>
+// 						<div class="info">
+// 							<p class="name">${data.name}</p>
+// 							<p class="pos">${data.position}</p>
+// 							<span class="dept">${data.dept}</span>
+// 						</div>
+// 					</article>
+// 				`;
+// 			});
+// 			contArea.innerHTML = tags;
+// 		}
+
+// 		//schedule data
+// 		const scheduleData = data.schedule;
+// 		if (scheduleData.length > 0) {
+// 			tags = '';
+// 			scheduleData.map((data) => {
+// 				tags += `
+// 					<li>
+// 						<p class="title">${data.subj}</p>
+// 						<div class="place">
+// 							<p>${data.date}</p>
+// 						</div>
+// 					</li>
+// 				`;
+// 			});
+// 			contLi.innerHTML = tags;
+// 		}
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
 const section = document.querySelectorAll('section')[1];
 const contArea = section.querySelector('.cont_area');
 const contLi = section.querySelector('ul');
 
-let tags;
+fetchData();
 
-fetch('DB/department.json')
-	.then((res) => {
-		return res.json();
-	})
-	.then((data) => {
-		//member data
-		const memberData = data.members;
+//데이터 fetching
+async function fetchData() {
+	const data = await fetch('DB/department.json');
+	const json = await data.json();
 
-		if (memberData.length > 0) {
-			tags = '';
-			memberData.map((data) => {
-				tags += `
-					<article>
-						<div class='pic'>
-							<img src='img/department/${data.pic}' />
-						</div>
-						<div class="info">
-							<p class="name">${data.name}</p>
-							<p class="pos">${data.position}</p>
-							<span class="dept">${data.dept}</span>
-						</div>
-					</article>
-				`;
-			});
-			contArea.innerHTML = tags;
-		}
+	console.log(json, 'json');
 
-		//schedule data
-		const scheduleData = data.schedule;
-		if (scheduleData.length > 0) {
-			tags = '';
-			scheduleData.map((data) => {
-				tags += `
-					<li>
-						<p class="title">${data.subj}</p>
-						<div class="place">
-							<p>${data.date}</p>
-						</div>
-					</li>
-				`;
-			});
-			contLi.innerHTML = tags;
-		}
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+	createDom(json);
+}
+
+//동적 생성
+function createDom(arr) {
+	let tags;
+	// console.log(arr);
+
+	//member data
+	const memberData = arr.members;
+	if (memberData.length > 0) {
+		tags = '';
+		memberData.map((data) => {
+			tags += `
+				<article>
+					<div class='pic'>
+						<img src='img/department/${data.pic}' />
+					</div>
+					<div class="info">
+						<p class="name">${data.name}</p>
+						<p class="pos">${data.position}</p>
+						<span class="dept">${data.dept}</span>
+					</div>
+				</article>
+			`;
+		});
+		contArea.innerHTML = tags;
+	}
+
+	//schedule data
+	const scheduleData = arr.schedule;
+	if (scheduleData.length > 0) {
+		tags = '';
+		scheduleData.map((data) => {
+			tags += `
+				<li>
+					<p class="title">${data.subj}</p>
+					<div class="place">
+						<p>${data.date}</p>
+					</div>
+				</li>
+			`;
+		});
+		contLi.innerHTML = tags;
+	}
+}
