17099fe330d018d95ca1fe8c520ea33ea4017540
choijiiyoung <blueberryxchoco@gmail.com>
2023-06-12 15:45:39 +0900

    갤러리 검색 기능 추가

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,24 @@
 const gallery = document.querySelector('.gallery .gallery_wrap');
+const input = document.querySelector('.gallery #search');
+const btnSearch = document.querySelector('.gallery .btn_search');
 const api_key = 'db5673d91b2fb6704d13f6b0181efd99';
 const num = 10;
 const myId = '198483448@N02';
 const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${api_key}&per_page=${num}&method=`;
 const method_interest = 'flickr.interestingness.getList';
 const method_user = 'flickr.people.getPhotos';
+const method_search = 'flickr.photos.search';
 const interest_url = `${baseURL}${method_interest}`;
 const user_url = `${baseURL}${method_user}&user_id=${myId}`;
 
 fetchData(interest_url);
+
+btnSearch.addEventListener('click', () => {
+	const inputVal = input.value.trim();
+	if (inputVal === '') alert('검색어를 입력해주세요.');
+	const url_search = `${baseURL}${method_search}&tags=${inputVal}`;
+	fetchData(url_search);
+});
 
 async function fetchData(url) {
 	const res = await fetch(url);
