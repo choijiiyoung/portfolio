b8f63b4a223035ec725d858fe71e2b02160dcc4c
choijiiyoung <blueberryxchoco@gmail.com>
2023-06-22 22:19:52 +0900

    anime.js 소스 수정버전 반영

--- js/anime.js
+++ js/anime.js
@@ -5,21 +5,22 @@ class Anime {
 		this.startTime = performance.now();
 		this.currentValue = null;
 
-		this.option.prop === 'scroll'
-			? (this.currentValue = this.selector.scrollY)
-			: (this.currentValue = parseFloat(getComputedStyle(this.selector)[this.option.prop]));
+		this.option.prop === 'scroll' ? (this.currentValue = this.selector.scrollY) : (this.currentValue = parseFloat(getComputedStyle(this.selector)[this.option.prop]));
 
 		this.isString = typeof this.option.value;
 		if (this.isString === 'string') {
 			const parentW = parseInt(getComputedStyle(this.selector.parentElement).width);
 			const parentH = parseInt(getComputedStyle(this.selector.parentElement).height);
-			const x = ['margin-left', 'margin-right', 'left', 'right', 'width'];
-			const y = ['margin-top', 'margin-bottom', 'top', 'bottom', 'height'];
 
-			for (const cond of x)
-				this.option.prop === cond && (this.currentValue = (this.currentValue / parentW) * 100);
-			for (const cond of y)
-				this.option.prop === cond && (this.currentValue = (this.currentValue / parentH) * 100);
+			const x = ['left', 'right', 'width'];
+			const y = ['top', 'bottom', 'height'];
+			const errProps = ['margin-left', 'margin-right', 'padding-left', 'padding-right', 'margin-top', 'margin-bottom', 'padding-top', 'padding-bottom'];
+
+			for (let cond of errProps) if (this.option.prop === cond) return console.error('margin, padding값은 퍼센트 모션처리할 수 없습니다.');
+
+			for (const cond of x) this.option.prop === cond && (this.currentValue = (this.currentValue / parentW) * 100);
+			for (const cond of y) this.option.prop === cond && (this.currentValue = (this.currentValue / parentH) * 100);
+
 			this.option.value = parseFloat(this.option.value);
 		}
 		this.option.value !== this.currentValue && requestAnimationFrame((time) => this.run(time));
@@ -30,9 +31,7 @@ class Anime {
 		let progress = timelast / this.option.duration || 500;
 		progress < 0 && (progress = 0);
 		progress > 1 && (progress = 1);
-		progress < 1
-			? requestAnimationFrame((time) => this.run(time))
-			: this.option.callback && setTimeout(this.option.callback, 0);
+		progress < 1 ? requestAnimationFrame((time) => this.run(time)) : this.option.callback && setTimeout(this.option.callback, 0);
 		let result = this.currentValue + (this.option.value - this.currentValue) * progress;
 
 		if (this.isString === 'string') this.selector.style[this.option.prop] = `${result}%`;
