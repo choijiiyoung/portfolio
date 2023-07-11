65e8044f0750f1c46c31175da668923714e4eaa1
choijiiyoung <blueberryxchoco@gmail.com>
2023-07-11 21:35:59 +0900

    deaprtment 수정

--- js/department.js
+++ js/department.js
@@ -1,51 +1,3 @@
-// fetch('DB/department.json')
-// 	.then((res) => {
-// 		return res.json();
-// 	})
-// 	.then((data) => {
-// 		//member data
-// 		const memberData = data.members;
-
-// 		if (memberData.length > 0) {
-// 			tags = '';
-// 			memberData.map((data) => {
-// 				tags += `
-// 					<article>
-// 						<div class='pic'>
-// 							<img src='img/department/${data.pic}' />
-// 						</div>
-// 						<div class="info">
-// 							<p class="name">${data.name}</p>
-// 							<p class="pos">${data.position}</p>
-// 							<span class="dept">${data.dept}</span>
-// 						</div>
-// 					</article>
-// 				`;
-// 			});
-// 			contArea.innerHTML = tags;
-// 		}
-
-// 		//schedule data
-// 		const scheduleData = data.schedule;
-// 		if (scheduleData.length > 0) {
-// 			tags = '';
-// 			scheduleData.map((data) => {
-// 				tags += `
-// 					<li>
-// 						<p class="title">${data.subj}</p>
-// 						<div class="place">
-// 							<p>${data.date}</p>
-// 						</div>
-// 					</li>
-// 				`;
-// 			});
-// 			contLi.innerHTML = tags;
-// 		}
-// 	})
-// 	.catch((err) => {
-// 		console.log(err);
-// 	});
-
 const section = document.querySelectorAll('section')[1];
 let tags;
 
@@ -55,8 +7,6 @@ fetchData();
 async function fetchData() {
 	const data = await fetch('DB/department.json');
 	const json = await data.json();
-
-	console.log(json, 'json');
 
 	createMember(json);
 	createSchedule(json);
@@ -90,7 +40,7 @@ function createSchedule(arr) {
 	tags = '';
 	const ul = document.createElement('ul');
 	const listArea = section.querySelector('.list_area');
-	const scheduleData = arr.schedule;
+	const scheduleData = arr.schedules;
 
 	scheduleData.forEach((data) => {
 		tags += `
